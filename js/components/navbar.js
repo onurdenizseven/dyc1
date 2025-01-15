1382bac80c57816c0278b49bc3807ae5b280668d
onurdenizseven <onur_denizseven@windowslive.com>
2025-01-15 19:09:39 +0300

    güncel

---
+++ js/components/navbar.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navButtons = document.querySelector('.nav-buttons');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+    navButtons.classList.toggle('active');
+});
+
+// Menü linklerine tıklandığında menüyü kapat
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+        navButtons.classList.remove('active');
+    });
+}); 
